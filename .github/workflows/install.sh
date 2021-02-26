6805e5de7527ffda682b652c4317f286b6dfdaa8
Xing Zhang <zhangxing.nju@gmail.com>
2021-02-25 17:15:51 -0800

    fix ci workflow

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 python -m pip install --upgrade pip
-pip install numpy scipy h5py pyscf jaxlib jax flax pytest
-export OMP_NUM_THREADS=1
-export PYTHONPATH=$(pwd):$PYTHONPATH
+pip install wheel
+pip install numpy scipy h5py 
+pip install pyscf 
+pip install jaxlib jax flax 
+pip install pytest
