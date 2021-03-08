94713e8998f925e6082eb0122dd9d303b33a7345
Xing Zhang <zhangxing.nju@gmail.com>
2021-03-08 14:31:08 -0800

    remove duplication

--- pyscfad/lib/np_helper.py
+++ pyscfad/lib/np_helper.py
@@ -1,15 +1,1 @@
-from jax import numpy as jnp
-
-array = jnp.array
-
-def asarray(*args, **kwargs):
-    return jnp.asarray(*args, **kwargs)
-
-def dot(*args, **kwargs):
-    return jnp.dot(*args, **kwargs)
-
-def einsum(*args, **kwargs):
-    return jnp.einsum(*args, **kwargs)
-
-def zeros(*args, **kwargs):
-    return jnp.zeros(*args, **kwargs)
+from jax.numpy import *
