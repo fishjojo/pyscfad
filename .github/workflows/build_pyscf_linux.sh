75cfc46fa9b54d65ec1c3c835d9bf2a439f97e4c
Xing Zhang <zhangxing.nju@gmail.com>
2021-07-07 20:43:52 -0700

    fix workflow

--- .github/workflows/build_pyscf_linux.sh
+++ .github/workflows/build_pyscf_linux.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 sudo apt-get -qq install \
     gcc \
-    gfortran \
-    libgfortran3 \
     libblas-dev \
-    cmake \
-    curl
+    cmake
 
 cd pyscf/pyscf/lib
-curl http://www.sunqm.net/pyscf/files/bin/pyscf-2.0a-deps.tar.gz | tar xzf -
+#curl http://www.sunqm.net/pyscf/files/bin/pyscf-2.0a-deps.tar.gz | tar xzf -
 mkdir build; cd build
-cmake -DBUILD_LIBXC=OFF -DBUILD_XCFUN=OFF -DBUILD_LIBCINT=OFF ..
+#cmake -DBUILD_LIBXC=OFF -DBUILD_XCFUN=OFF -DBUILD_LIBCINT=OFF ..
 cmake ..
 make -j4
