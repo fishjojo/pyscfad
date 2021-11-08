263bdc4495ae2e5903ea44ed05ea1fffe239c4ed
Xing Zhang <zhangxing.nju@gmail.com>
2021-11-07 23:52:05 -0800

    deprecated index function

--- pyscfad/lib/ops.py
+++ pyscfad/lib/ops.py
@@ -5,7 +5,7 @@ from jax import numpy as jnp
 from jax.config import config as jax_config
 jax_config.update("jax_enable_x64", True)
 
-index = jax.ops.index
+index = jnp.index_exp
 if LooseVersion(jax.__version__) < '0.2.22':
     index_update = jax.ops.index_update
     index_add = jax.ops.index_add
