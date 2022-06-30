ea24c48e17dc4e4d967f1e59160b11cacd6293d9
Xing Zhang <zhangxing.nju@gmail.com>
2022-06-30 01:19:55 -0700

    fix workflow

--- .github/workflows/run_test.sh
+++ .github/workflows/run_test.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 export OMP_NUM_THREADS=1
 export PYTHONPATH=$(pwd):$(pwd)/pyscf:$PYTHONPATH
+# preload MKL
+if [[ -n "${MKLROOT}" ]]; then
+  export LD_PRELOAD=$MKLROOT/lib/intel64_lin/libmkl_avx2.so:$MKLROOT/lib/intel64_lin/libmkl_sequential.so:$MKLROOT/lib/intel64_lin/libmkl_core.so
+fi
 echo "pyscfad = True" >> $HOME/.pyscf_conf.py
 echo "pyscf_numpy_backend = 'jax'" >> $HOME/.pyscf_conf.py
 echo "pyscf_scipy_linalg_backend = 'pyscfad'" >> $HOME/.pyscf_conf.py
@@ -9,5 +13,4 @@ echo "pyscfad_scf_implicit_diff = True" >> $HOME/.pyscf_conf.py
 echo "pyscfad_ccsd_implicit_diff = True" >> $HOME/.pyscf_conf.py
 
 cd pyscfad
-#pytest --cov-report xml --cov=. --verbosity=1 --durations=10
-pytest dft
+pytest --cov-report xml --cov=. --verbosity=1 --durations=10
