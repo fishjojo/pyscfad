fix pbc dft gradient

pyscfad/pbc/dft/krks.py
@@ -1,10 +1,9 @@
-from typing import Optional
 import numpy
 from pyscf import __config__
 from pyscf.lib import logger
 from pyscf.pbc.dft import krks as pyscf_krks
 from pyscf.pbc.dft import gen_grid, multigrid
-from pyscfad import lib
+from pyscfad import util
 from pyscfad.lib import numpy as np
 from pyscfad.lib import stop_grad
 from pyscfad.dft.rks import VXC
@@ -16,7 +15,8 @@ def get_veff(ks, cell=None, dm=None, dm_last=0, vhf_last=0, hermi=1,
     if cell is None: cell = ks.cell
     if dm is None: dm = ks.make_rdm1()
     if kpts is None: kpts = ks.kpts
-    t0 = (logger.process_clock(), logger.perf_counter())
+
+    log = logger.new_logger(ks)
 
     omega, alpha, hyb = ks._numint.rsh_and_hybrid_coeff(ks.xc, spin=cell.spin)
     hybrid = abs(hyb) > 1e-10 or abs(alpha) > 1e-10
@@ -25,8 +25,8 @@ def get_veff(ks, cell=None, dm=None, dm_last=0, vhf_last=0, hermi=1,
         n, exc, vxc = multigrid.nr_rks(ks.with_df, ks.xc, dm, hermi,
                                        kpts, kpts_band,
                                        with_j=True, return_j=False)
-        logger.debug(ks, 'nelec by numeric integration = %s', n)
-        t0 = logger.timer(ks, 'vxc', *t0)
+        log.debug('nelec by numeric integration = %s', n)
+        log.timer('vxc')
         return vxc
 
     # ndim = 3 : dm.shape = (nkpts, nao, nao)
@@ -39,15 +39,15 @@ def get_veff(ks, cell=None, dm=None, dm_last=0, vhf_last=0, hermi=1,
         if (isinstance(ks.grids, gen_grid.BeckeGrids) and
             ks.small_rho_cutoff > 1e-20 and ground_state):
             ks.grids = rks.prune_small_rho_grids_(ks, stop_grad(cell), stop_grad(dm), ks.grids, kpts)
-        t0 = logger.timer(ks, 'setting up grids', *t0)
+        log.timer('setting up grids')
 
     if hermi == 2:  # because rho = 0
         n, exc, vxc = 0, 0, 0
     else:
         n, exc, vxc = ks._numint.nr_rks(cell, ks.grids, ks.xc, dm, 0,
                                         kpts, kpts_band)
-        logger.debug(ks, 'nelec by numeric integration = %s', n)
-        t0 = logger.timer(ks, 'vxc', *t0)
+        log.debug('nelec by numeric integration = %s', n)
+        log.timer('vxc')
 
     weight = 1./len(kpts)
     if not hybrid:
@@ -73,21 +73,21 @@ def get_veff(ks, cell=None, dm=None, dm_last=0, vhf_last=0, hermi=1,
         ecoul = None
 
     vxc = VXC(vxc=vxc, ecoul=ecoul, exc=exc, vj=None, vk=None)
+    del log
     return vxc
 
-@lib.dataclass
+@util.pytree_node(khf.Traced_Attributes, num_args=1)
 class KRKS(rks.KohnShamDFT, khf.KRHF):
     def __init__(self, cell, kpts=numpy.zeros((1,3)), xc='LDA,VWN',
                  exxdiv=getattr(__config__, 'pbc_scf_SCF_exxdiv', 'ewald'), **kwargs):
-        self.cell = cell
-        self.xc = xc
-        self.kpts = kpts
-        self.exxdiv = exxdiv
-        for key in kwargs.keys():
-            setattr(self, key, kwargs[key])
-        if not self._built:
-            khf.KRHF.__init__(self, cell, kpts=kpts, exxdiv=exxdiv)
-            rks.KohnShamDFT.__init__(self, xc)
+        khf.KRHF.__init__(self, cell, kpts, exxdiv, **kwargs)
+        rks.KohnShamDFT.__init__(self, xc)
+        self.__dict__.update(kwargs)
+        # NOTE this has to be after __dict__ update,
+        # otherwise stop_grad(mol) won't work.
+        # Currently, no grid response is considered.
+        rks.KohnShamDFT.__post_init__(self)
+
 
     def dump_flags(self, verbose=None):
         khf.KRHF.dump_flags(self, verbose)
@@ -105,9 +105,9 @@ class KRKS(rks.KohnShamDFT, khf.KRHF):
         weight = 1./len(h1e_kpts)
         e1 = weight * np.einsum('kij,kji', h1e_kpts, dm_kpts)
         tot_e = e1 + vhf.ecoul + vhf.exc
-        self.scf_summary['e1'] = e1.real
-        self.scf_summary['coul'] = vhf.ecoul.real
-        self.scf_summary['exc'] = vhf.exc.real
+        self.scf_summary['e1'] = stop_grad(e1.real)
+        self.scf_summary['coul'] = stop_grad(vhf.ecoul.real)
+        self.scf_summary['exc'] = stop_grad(vhf.exc.real)
         logger.debug(self, 'E1 = %s  Ecoul = %s  Exc = %s', e1, vhf.ecoul, vhf.exc)
         return tot_e.real, vhf.ecoul + vhf.exc
 