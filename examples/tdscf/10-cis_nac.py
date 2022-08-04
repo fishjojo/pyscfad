f69e185a5f1e6fbb49be41b340ff584871402a53
fishjojo <zhangxing.nju@gmail.com>
2022-08-03 20:18:11 -0700

    add examples

--- examples/tdscf/10-cis_nac.py
+++ examples/tdscf/10-cis_nac.py
@@ -1,34 +1,43 @@
+import numpy
 import jax
-from jax import numpy as jnp
-from pyscfad import gto, scf, tdscf
+from pyscfad import gto, scf
+from pyscfad.tdscf.rhf import CIS, cis_ovlp
 
+# molecular structure
 mol = gto.Mole()
 mol.atom = 'H 0 0 0; H 0 0 1.1'
-mol.basis = 'ccpvtz'
-mol.verbose = 4
-mol.build(trace_exp=False, trace_ctr_coeff=False)
+mol.basis = 'cc-pvdz'
+mol.build()
 
+# HF and CIS calculations
 mf = scf.RHF(mol)
 mf.kernel()
-mytd = tdscf.rhf.CIS(mf)
-mytd.nstates = 3
+mytd = CIS(mf)
+mytd.nstates = 8
 e, xy = mytd.kernel()
 
-i, j = 0, 2
-xi = xy[i][0] * jnp.sqrt(2.)
-xj = xy[j][0] * jnp.sqrt(2.)
+# Target excited states I and J (1st and 4th)
+stateI, stateJ = 0, 2
+# CI coefficients of state I
+xi = xy[stateI][0] * numpy.sqrt(2.)
+nmo = mf.mo_coeff.shape[-1]
+nocc = mol.nelectron // 2
 
-# Using Hellman-Feynman formalism.
-# The amplitude is closed over, so there is no tracing through the Davidson iteration.
-def hellman(mol):
-    mf = scf.RHF(mol)
-    mf.kernel()
-    mytd = tdscf.rhf.CIS(mf)
-    mytd.nstates = 3
+def ovlp(mol1):
+    mf1 = scf.RHF(mol1)
+    mf1.kernel()
+    mytd1 = CIS(mf1)
+    mytd1.nstates = 8
+    _, xy1 = mytd1.kernel()
+    # CI coefficients of state J
+    xj = xy1[stateJ][0] * numpy.sqrt(2.)
+    # CIS wavefunction overlap
+    s = cis_ovlp(mol, mol1, mf.mo_coeff, mf1.mo_coeff,
+                 nocc, nocc, nmo, nmo, xi, xj)
+    return s
 
-    vind, _ = mytd.gen_vind(mytd._scf)
-    e = jnp.dot(xi.ravel(), vind(xj).ravel())
-    return e
-
-nac = jax.grad(hellman)(mol)
-print(nac.coords / (e[j]-e[i]))
+# Only the ket state is differentiated
+mol1 = mol.copy()
+jac = jax.jacrev(ovlp)(mol1)
+print("CIS derivative coupling:")
+print(jac.coords)
