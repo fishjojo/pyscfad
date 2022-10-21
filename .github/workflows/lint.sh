577f004b9929bef5c077876700df8f4e1d572226
Xing Zhang <zhangxing.nju@gmail.com>
2022-10-21 13:19:51 -0700

    fix lint workflow

--- .github/workflows/lint.sh
+++ .github/workflows/lint.sh
@@ -2,5 +2,8 @@
 export OMP_NUM_THREADS=1
 export PYTHONPATH=$(pwd):$(pwd)/pyscf:$PYTHONPATH
 echo "pyscfad = True" >> $HOME/.pyscf_conf.py
+echo "pyscf_numpy_backend = 'jax'" >> $HOME/.pyscf_conf.py
+echo "pyscf_scipy_linalg_backend = 'pyscfad'" >> $HOME/.pyscf_conf.py
+echo "pyscf_scipy_backend = 'jax'" >> $HOME/.pyscf_conf.py
 
 pylint pyscfad
