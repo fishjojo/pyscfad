71494cf408e598c2f92af3e22e185d104756fe78
Xing Zhang <zhangxing.nju@gmail.com>
2023-01-25 13:05:52 -0800

    enforce incore density fitting

--- pyscfad/df/df.py
+++ pyscfad/df/df.py
@@ -29,13 +29,13 @@ class DF(pyscf_df.DF):
         max_memory = self.max_memory - pyscf_lib.current_memory()[0]
         int3c = mol._add_suffix('int3c2e')
         int2c = mol._add_suffix('int2c2e')
-        if (nao_pair*naux*8/1e6 < .9*max_memory and
-            not isinstance(self._cderi_to_save, str)):
-            self._cderi = incore.cholesky_eri(mol, auxmol=auxmol,
-                                              int3c=int3c, int2c=int2c,
-                                              max_memory=max_memory, verbose=log)
-        else:
-            raise NotImplementedError('Outcore density fitting is not implemented.')
+        if (nao_pair*naux*8/1e6 > .9*max_memory or
+            isinstance(self._cderi_to_save, str)):
+            log.warn('Outcore density fitting is not implemented.\n'
+                     'Using incore density fitting instead.')
+        self._cderi = incore.cholesky_eri(mol, auxmol=auxmol,
+                                          int3c=int3c, int2c=int2c,
+                                          max_memory=max_memory, verbose=log)
         del log
         return self
 
