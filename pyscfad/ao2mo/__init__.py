80ca385fc2d9399b037c207600381b3e3eca4e63
Xing Zhang <zhangxing.nju@gmail.com>
2023-04-07 17:55:49 -0700

    minor fix

--- pyscfad/ao2mo/__init__.py
+++ pyscfad/ao2mo/__init__.py
@@ -1,7 +1,8 @@
 from pyscfad.lib import isarray
 from pyscfad.ao2mo import incore
 
-def general(eri_or_mol, mo_coeffs, erifile=None, dataname='eri_mo', intor='int2e',
+def general(eri_or_mol, mo_coeffs, *args,
+            erifile=None, dataname='eri_mo', intor='int2e',
             **kwargs):
     if isarray(eri_or_mol):
         return incore.general(eri_or_mol, mo_coeffs, *args, **kwargs)
