16cc3055572d0980e3ef886e77fa4f11ba1abce9
fishjojo <zhangxing.nju@gmail.com>
2023-08-28 22:29:55 -0700

    update lib install

--- pyscfadlib/apply_patch.sh
+++ pyscfadlib/apply_patch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if ! out=$(patch -p1 --forward < $( dirname -- "${BASH_SOURCE[0]}" )/pyscf.patch)
+if ! out=$(patch -p1 --forward < $( dirname -- "${BASH_SOURCE[0]}" )/$1)
 then
     if echo "$out" | grep -q "Reversed (or previously applied) patch detected!  Skipping patch."
     then
