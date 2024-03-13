9888b512aeaa254d4c78b310a2c158ef65f365a4
Xing Zhang <zhangxing.nju@gmail.com>
2024-03-13 15:12:19 -0700

    update example

--- examples/scf/10-polarizability.py
+++ examples/scf/10-polarizability.py
@@ -1,7 +1,7 @@
 import numpy
 import jax
+from pyscfad import numpy as np
 from pyscfad import gto, scf
-from pyscfad.lib import numpy as jnp
 
 mol = gto.Mole()
 mol.atom = '''H  ,  0.   0.   0.
@@ -15,7 +15,7 @@ ao_dip = mol.intor_symmetric('int1e_r', comp=3)
 h1 = mf.get_hcore()
     
 def apply_E(E):
-    mf.get_hcore = lambda *args, **kwargs: h1 + jnp.einsum('x,xij->ij', E, ao_dip)
+    mf.get_hcore = lambda *args, **kwargs: h1 + np.einsum('x,xij->ij', E, ao_dip)
     mf.kernel()
     return mf.dip_moment(mol, mf.make_rdm1(), unit='AU', verbose=0)
 
@@ -24,7 +24,7 @@ polar = jax.jacfwd(apply_E)(E0)
 print(polar)
 
 def apply_E1(E):
-    mf.get_hcore = lambda *args, **kwargs: h1 + jnp.einsum('x,xij->ij', E, ao_dip)
+    mf.get_hcore = lambda *args, **kwargs: h1 + np.einsum('x,xij->ij', E, ao_dip)
     return mf.kernel()
 
 polar = -jax.hessian(apply_E1)(E0)
