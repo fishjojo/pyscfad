b2c8463465bd6c7198cfee36a3239645fb9d4be0
fishjojo <zhangxing.nju@gmail.com>
2024-03-15 18:08:17 -0700

    add mpi geomety opt

--- pyscfad/geomopt/__init__.py
+++ pyscfad/geomopt/__init__.py
@@ -5,3 +5,10 @@ def optimize(*args, **kwargs):
     except ImportError as err:
         raise ImportError('Unable to import geometric.') from err
     return geom.kernel(*args, **kwargs)
+
+def optimize_mpi(*args, **kwargs):
+    try:
+        from . import geometric_solver_mpi as geom
+    except ImportError as err:
+        raise ImportError('Unable to import geometric.') from err
+    return geom.kernel(*args, **kwargs)
