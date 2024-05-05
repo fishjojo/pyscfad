77a3af6d473dc5b243be4844c493fefa8917b78d
Xing Zhang <zhangxing.nju@gmail.com>
2024-05-04 19:05:12 -0700

    test

--- pyscfadlib/setup.py
+++ pyscfadlib/setup.py
@@ -13,10 +13,13 @@ class CMakeBuild(build_py):
     def run(self):
         if sys.platform == 'darwin':
             platform = os.getenv('_PYTHON_HOST_PLATFORM')
+            print('debug: ', platform)
             if platform.endswith('arm64'):
                 os.putenv('CMAKE_OSX_ARCHITECTURES', 'arm64')
+                print('debug: arm64')
             elif platform.endswith('x86_64'):
                 os.putenv('CMAKE_OSX_ARCHITECTURES', 'x86_64')
+                print('debug: x86_64')
 
         self.plat_name = get_platform()
         self.build_base = 'build'
