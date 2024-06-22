bb48be4ccb3df7458ad9723c27168f2e78d30f4b
fishjojo <zhangxing.nju@gmail.com>
2024-06-22 00:16:31 -0700

    add publish workflow

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         'jax>=0.3.25',
         'jaxlib>=0.3.25',
         'pyscf>=2.3',
-        'pyscfadlib==0.1.4',
+        'pyscfadlib>=0.1.4',
         #'pyscf-properties @ git+https://github.com/fishjojo/properties.git@ad',
     ],
     url='https://github.com/fishjojo/pyscfad',
