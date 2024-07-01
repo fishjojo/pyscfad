4b6291b1be097d8d4341646a95fed7e427e8e48d
fishjojo <zhangxing.nju@gmail.com>
2024-06-30 20:04:32 -0700

    minor update df.incore

--- pyscfad/df/df.py
+++ pyscfad/df/df.py
@@ -5,7 +5,7 @@ from pyscf import lib as pyscf_lib
 from pyscf.lib import logger
 from pyscf.df import df as pyscf_df
 from pyscfad import util
-from pyscfad.ops import isarray
+from pyscfad.ops import is_array
 from pyscfad.df import addons, incore, df_jk
 
 @util.pytree_node(['mol', 'auxmol', '_cderi'], num_args=1)
@@ -77,8 +77,8 @@ class DF(pyscf_df.DF):
         else:
             raise NotImplementedError
 
-    def loop(self, blksize=None, convert_to_ndarray=True):
-        # NOTE By default (convert_to_ndarray=True)
+    def loop(self, blksize=None, to_numpy=True):
+        # NOTE By default (to_numpy=True)
         # we do not trace this function so that it can be used by pyscf
         if self._cderi is None:
             self.build()
@@ -86,10 +86,10 @@ class DF(pyscf_df.DF):
             blksize = self.blockdim
 
         with addons.load(self._cderi, 'j3c') as feri:
-            if isarray(feri):
+            if is_array(feri):
                 naoaux = feri.shape[0]
                 for b0, b1 in self.prange(0, naoaux, blksize):
-                    if convert_to_ndarray:
+                    if to_numpy:
                         out = numpy.asarray(feri[b0:b1], order='C')
                     else:
                         out = feri[b0:b1]
--- pyscfad/df/incore.py
+++ pyscfad/df/incore.py
@@ -1,18 +1,21 @@
 from functools import partial
 from jax import scipy
-from jax import custom_jvp
-from jax import numpy as np
-from pyscf.lib import logger
-from pyscf import gto
-from pyscf.df.outcore import _guess_shell_ranges
 from pyscf import __config__
+from pyscf.df.outcore import _guess_shell_ranges
+from pyscfad import numpy as np
 from pyscfad import ops
-from pyscfad.ops import vmap, jit
+from pyscfad.ops import (
+    custom_jvp,
+    vmap,
+    jit,
+)
 from pyscfad import config
+from pyscfad.lib import logger
+from pyscfad.gto import _pyscf_moleintor as moleintor
 from . import addons, _int3c_cross_opt
 
 MAX_MEMORY = getattr(__config__, 'df_outcore_max_memory', 2000)
-LINEAR_DEP_THR = getattr(__config__, 'df_df_DF_lindep', 1e-12)
+LINEAR_DEP_THR = getattr(__config__, 'df_df_DF_lindep', 1e-7)
 
 @partial(custom_jvp, nondiff_argnums=tuple(range(2,7)))
 def int3c_cross(mol, auxmol, intor='int3c2e', comp=1, aosym='s1',
@@ -22,23 +25,24 @@ def int3c_cross(mol, auxmol, intor='int3c2e', comp=1, aosym='s1',
     atm = pmol._atm
     bas = pmol._bas
     env = pmol._env
-    int3c = gto.moleintor.ascint3(mol._add_suffix(intor))
-    ao_loc = gto.moleintor.make_loc(bas, int3c)
-    cintopt = gto.moleintor.make_cintopt(atm, bas, env, int3c)
+    int3c = moleintor.ascint3(mol._add_suffix(intor))
+    ao_loc = moleintor.make_loc(bas, int3c)
+    cintopt = moleintor.make_cintopt(atm, bas, env, int3c)
     nbas = mol.nbas
     nauxbas = auxmol.nbas
     if shls_slice is None:
         shls_slice = (0, nbas, 0, nbas, nbas, nbas+nauxbas)
-    ints = gto.moleintor.getints3c(int3c, atm, bas, env,
-                                   shls_slice=shls_slice, comp=comp,
-                                   aosym=aosym, ao_loc=ao_loc, cintopt=cintopt, out=out)
+    ints = moleintor.getints3c(int3c, atm, bas, env,
+                               shls_slice=shls_slice, comp=comp,
+                               aosym=aosym, ao_loc=ao_loc,
+                               cintopt=cintopt, out=out)
     return ints
 
 @int3c_cross.defjvp
 def int3c_cross_jvp(intor, comp, aosym, shls_slice, out,
                     primals, tangents):
     mol, auxmol = primals
-    mol_dot, auxmol_dot = tangents
+    mol_t, auxmol_t = tangents
     assert shls_slice[0] == 0 and shls_slice[1] == mol.nbas
 
     primal_out = int3c_cross(mol, auxmol, intor=intor, comp=comp,
@@ -49,13 +53,12 @@ def int3c_cross_jvp(intor, comp, aosym, shls_slice, out,
         intor_ip1 = intor.replace('int3c2e', 'int3c2e_ip1')
         ints = int3c_cross(mol, auxmol, intor=intor_ip1, comp=3,
                            aosym=aosym, shls_slice=shls_slice)
-        tangent_out_mol = _int3c_fill_grad_r0_ip1(mol, mol_dot, -ints)
+        tangent_out += _int3c_fill_jvp_r0_ip1(mol, mol_t, -ints)
 
         intor_ip2 = intor.replace('int3c2e', 'int3c2e_ip2')
         ints = int3c_cross(mol, auxmol, intor=intor_ip2, comp=3,
                            aosym=aosym, shls_slice=shls_slice)
-        tangent_out_auxmol = _int3c_fill_grad_r0_ip2(auxmol, auxmol_dot, -ints)
-        tangent_out += tangent_out_mol + tangent_out_auxmol
+        tangent_out += _int3c_fill_jvp_r0_ip2(auxmol, auxmol_t, -ints)
     else:
         raise NotImplementedError
 
@@ -66,45 +69,29 @@ def int3c_cross_jvp(intor, comp, aosym, shls_slice, out,
     return primal_out, tangent_out
 
 @jit
-def _int3c_fill_grad_r0_ip1(mol, mol_dot, ints):
-    shape = [mol.natm,] + list(ints.shape)
-    grad = np.zeros(shape, dtype=ints.dtype)
-    aoslices = mol.aoslice_by_atom()
-    shape = [1,] * ints.ndim
-    shape[1] = ints.shape[1]
-    idx = np.arange(shape[1])
-    idx = idx.reshape(shape)
-    #for ia in range(mol.natm):
-    #    p0, p1 = aoslices[ia,2:]
-    #    grad = ops.index_update(grad, ops.index[ia,:,p0:p1], ints[:,p0:p1])
-    def body(slices):
-        p0, p1 = slices[:]
-        mask = (idx >= p0) & (idx < p1)
-        return np.where(mask, ints, np.array(0, dtype=ints.dtype))
-    grad = vmap(body)(aoslices[:,2:].reshape(-1,2))
-    tangent_out = np.einsum('nxijk,nx->ijk', grad, mol_dot.coords)
-    tangent_out += tangent_out.transpose(1,0,2)
-    return tangent_out
+def _int3c_fill_jvp_r0_ip1(mol, mol_t, ints):
+    naoi = ints.shape[1]
+    idx = np.arange(naoi)[None,:,None,None]
+    aoslices = mol.aoslice_by_atom()[:,2:4]
+    def _fill(sl, coord):
+        mask = (idx >= sl[0]) & (idx < sl[1])
+        grad = np.where(mask, ints, np.array(0, dtype=ints.dtype))
+        return np.einsum('xijk,x->ijk', grad, coord)
+    jvp = np.sum(vmap(_fill)(aoslices, mol_t.coords), axis=0)
+    jvp += jvp.transpose(1,0,2)
+    return jvp
 
 @jit
-def _int3c_fill_grad_r0_ip2(mol, mol_dot, ints):
-    shape = [mol.natm,] + list(ints.shape)
-    grad = np.zeros(shape, dtype=ints.dtype)
-    aoslices = mol.aoslice_by_atom()
-    shape = [1,] * ints.ndim
-    shape[-1] = ints.shape[-1]
-    idx = np.arange(shape[-1])
-    idx = idx.reshape(shape)
-    #for ia in range(mol.natm):
-    #    p0, p1 = aoslices[ia,2:]
-    #    grad = ops.index_update(grad, ops.index[ia,...,p0:p1], ints[...,p0:p1])
-    def body(slices):
-        p0, p1 = slices[:]
-        mask = (idx >= p0) & (idx < p1)
-        return np.where(mask, ints, np.array(0, dtype=ints.dtype))
-    grad = vmap(body)(aoslices[:,2:].reshape(-1,2))
-    tangent_out = np.einsum('nxijk,nx->ijk', grad, mol_dot.coords)
-    return tangent_out
+def _int3c_fill_jvp_r0_ip2(mol, mol_t, ints):
+    naux = ints.shape[-1]
+    idx = np.arange(naux)[None,None,None,:]
+    aoslices = mol.aoslice_by_atom()[:,2:4]
+    def _fill(sl, coord):
+        mask = (idx >= sl[0]) & (idx < sl[1])
+        grad = np.where(mask, ints, np.array(0, dtype=ints.dtype))
+        return np.einsum('xijk,x->ijk', grad, coord)
+    jvp = np.sum(vmap(_fill)(aoslices, mol_t.coords), axis=0)
+    return jvp
 
 
 def cholesky_eri(mol, auxmol=None, auxbasis='weigend+etb',
@@ -127,8 +114,8 @@ def cholesky_eri(mol, auxmol=None, auxbasis='weigend+etb',
     try:
         low = scipy.linalg.cholesky(j2c, lower=True)
         tag = 'cd'
-    # pylint: disable=bare-except
-    except: #scipy.linalg.LinAlgError:
+    # pylint: disable=broad-exception-caught
+    except Exception:
         w, v = scipy.linalg.eigh(j2c)
         idx = w > LINEAR_DEP_THR
         low = (v[:,idx] / np.sqrt(w[idx]))
@@ -145,24 +132,21 @@ def cholesky_eri(mol, auxmol=None, auxbasis='weigend+etb',
     else:
         nao_pair = nao * (nao+1) // 2
 
-    cderi = np.empty((naux, nao_pair))
+    cderi = np.zeros((naux, 0), dtype=float)
 
-    max_words = max_memory*1e6/8 - low.size - cderi.size
-    # Divide by 3 because scipy.linalg.solve may create a temporary copy for
-    # ints and return another copy for results
-    buflen = min(max(int(max_words/naoaux/comp/3), 8), nao_pair)
+    max_words = max_memory*1e6/8 - low.size - naux*nao_pair
+    buflen = min(max(int(max_words/naoaux/comp/2), 8), nao_pair)
     if not config.moleintor_opt:
-        # subshells not supported
+        # NOTE sub-shells not supported
         buflen = nao_pair
     shranges = _guess_shell_ranges(mol, buflen, aosym)
     log.debug1('shranges = %s', shranges)
 
-    p1 = 0
     for istep, sh_range in enumerate(shranges):
         log.debug('int3c2e [%d/%d], AO [%d:%d], nrow = %d',
                   istep+1, len(shranges), *sh_range)
 
-        bstart, bend, nrow = sh_range
+        bstart, bend, _ = sh_range
         shls_slice = (bstart, bend, 0, mol.nbas, mol.nbas, mol.nbas+auxmol.nbas)
 
         if config.moleintor_opt:
@@ -174,15 +158,13 @@ def cholesky_eri(mol, auxmol=None, auxbasis='weigend+etb',
                                shls_slice=shls_slice)
         ints = ints.reshape((-1,naoaux)).T
 
-        p0, p1 = p1, p1 + nrow
         if tag == 'cd':
-            cderi = ops.index_update(cderi, ops.index[:,p0:p1],
-                                     scipy.linalg.solve_triangular(
-                                        low, ints, lower=True,
-                                        overwrite_b=True, check_finite=False))
+            _eri = scipy.linalg.solve_triangular(
+                        low, ints, lower=True,
+                        overwrite_b=True, check_finite=False)
         else:
-            cderi = ops.index_update(cderi, ops.index[:,p0:p1],
-                                     np.dot(low.T, ints))
+            _eri = np.dot(low.T, ints)
+        cderi = np.append(cderi, _eri, axis=1)
 
     log.timer('cholesky_eri', *t0)
     del log
