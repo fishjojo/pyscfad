0e1140cd79cb74665ff77e3837066fc5315abb92
fishjojo <zhangxing.nju@gmail.com>
2025-02-17 17:06:45 -0800

    test

--- .github/workflows/run_test.sh
+++ .github/workflows/run_test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 export PYTHONPATH=$(pwd):$(pwd)/pyscfadlib:$PYTHONPATH
+export OMP_NUM_THREADS=1
 
-pytest ./pyscfad --cov-report xml --cov=. --verbosity=1 --durations=10
+#pytest ./pyscfad --cov-report xml --cov=. --verbosity=1 --durations=10
+pytest ./pyscfad/dft/test_uks.py
