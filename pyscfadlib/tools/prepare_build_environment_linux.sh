50aa5a28cd16781cb3b98d8aada43ab49474a865
fishjojo <zhangxing.nju@gmail.com>
2025-03-12 19:33:13 -0700

    test

--- pyscfadlib/tools/prepare_build_environment_linux.sh
+++ pyscfadlib/tools/prepare_build_environment_linux.sh
@@ -2,7 +2,10 @@
 
 set -ex
 
-if [ "$CIBW_ARCHS" == "x86_64" ]; then
+echo $CIBW_ARCHS
+echo $CIBW_ARCHS_LINUX
+
+if [ "$CIBW_ARCHS_LINUX" == "x86_64" ]; then
     # Install CUDA Toolkit
     yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo
     yum install -y cuda-toolkit-12-5-12.5.1-1
