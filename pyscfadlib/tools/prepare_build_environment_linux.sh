8b9999cfa5917634cd6d858d34b7fd27e0875589
fishjojo <zhangxing.nju@gmail.com>
2025-03-12 20:26:27 -0700

    test

--- pyscfadlib/tools/prepare_build_environment_linux.sh
+++ pyscfadlib/tools/prepare_build_environment_linux.sh
@@ -5,8 +5,8 @@ set -ex
 #if [ "$CIBW_ARCHS_LINUX" == "x86_64" ]; then
 # Install CUDA Toolkit
 yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
-yum install -y cuda-toolkit-12-12.4.1-1
-export PATH=/usr/local/cuda/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+yum install -y cuda-toolkit
+#export PATH=/usr/local/cuda/bin:$PATH
+#export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 nvcc --version
 #fi
