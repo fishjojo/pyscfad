2c1e559882b4e5053dfa8c84a0937f4ecfd80844
fishjojo <zhangxing.nju@gmail.com>
2025-03-13 17:48:13 -0700

    test

--- pyscfadlib/tools/prepare_build_environment_linux.sh
+++ pyscfadlib/tools/prepare_build_environment_linux.sh
@@ -10,5 +10,9 @@ yum-config-manager --enable epel
 yum install -y openblas-devel
 yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo
 #yum install -y cuda-toolkit
-yum install -y cuda-nvcc-12-8-12.8.93-1
-yum install -y cuda-cudart-devel-12-8-12.8.90-1
+yum install -y \
+    cuda-nvcc-12-8-12.8.93-1 \
+    cuda-cudart-devel-12-8-12.8.90-1 \
+    libcublas-devel-12-8-12.8.4.1-1 \
+    libcusolver-devel-12-8-11.7.3.90-1 \
+    libnvjitlink-devel-12-8-12.8.93-1
